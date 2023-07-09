4ee4764ccbc1ffb436e4abc36cf8ad783effee90
Santiago Ospina <santiagoospinarojas27@gmail.com>
2023-07-08 22:39:58 -0500

    fix: in function jwt

--- src/helpers/Jwt.js
+++ src/helpers/Jwt.js
@@ -1,24 +1,23 @@
 import jwt from "jsonwebtoken";
 import "dotenv/config";
-  class Jwt {
-    static createJWT(datos) {
-      const timer = Date.now() + 60 * 1000 * 1;
-      return jwt.sign({ datos, exp: timer }, process.env.SECRET_KEY);
-    }
+class Jwt {
+  static createJWT(datos) {
+    return jwt.sign({ datos }, process.env.SECRET_KEY, { expiresIn:process.env.EXPIRE_TOKEN });
+  }
 
-    static verifyAuthJWT(req, res, next) {
-      try {
-        const token = req.headers.authorization.split(" ")[1];
-        const payload = jwt.verify(token, process.env.SECRET_KEY);
-        if (Date.now() > payload.exp) {
-          return res.error("Token expirado");
-        }
-       return next();
-      } catch (error) {
-        return res.error("No existe token", error);
+  static verifyAuthJWT(req, res, next) {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.SECRET_KEY);
+      return next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.error("Token expirado");
       }
+      return res.error("No existe token o token inválido", error);
     }
-  };
+  }
+}
 
 export default Jwt;
 
